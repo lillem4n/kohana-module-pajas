1a8eda47b98e21e1e7b3fe2013219af180017dde
Lilleman <lilleman@larvit.se>
2013-02-16 17:33:00 +0100

    Fixed compability with welcome to generic controller

--- classes/controller/welcome.php
+++ classes/controller/welcome.php
@@ -1,3 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-class Controller_Welcome extends Controller_Generic {}
+class Controller_Welcome extends Controller_Generic
+{
+	public $xslt_stylesheet = 'generic';
+}
