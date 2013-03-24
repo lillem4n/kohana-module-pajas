178b5f0e0c7eaa937a071c8445cbbd679c6c3fb5
Lilleman <lilleman@larvit.se>
2013-03-24 16:10:10 +0100

    Updated image resizing scripts

--- classes/controller/media.php
+++ classes/controller/media.php
@@ -55,38 +55,93 @@ class Controller_Media extends Controller
 	{
 		$path      = $this->request->param('path');
 		$path_info = pathinfo($path);
+		$mime      = File::mime_by_ext($path_info['extension']);
 
 		$file = Kohana::find_file('img', substr($path, 0, strlen($path) - (strlen($path_info['extension']) + 1)), $path_info['extension']);
-		if ($file)
+		if ($file && substr($mime, 0, 5) == 'image')
 		{
-			$mime = File::mime_by_ext($path_info['extension']);
-			if (substr($mime, 0, 5) == 'image')
+			// Find the file ending
+			$file_ending = pathinfo($file, PATHINFO_EXTENSION);
+
+			// Check if it needs resizing
+			$cache_ending = '';
+
+			list($original_width, $original_height) = getimagesize($file);
+			$wh_ratio = $original_width / $original_height;
+
+			// Get params
+			if ( ! (isset($_GET['width'])     && preg_match('/^\d+$/', $_GET['width']    ))) $_GET['width']     = FALSE;
+			if ( ! (isset($_GET['height'])    && preg_match('/^\d+$/', $_GET['height']   ))) $_GET['height']    = FALSE;
+			if ( ! (isset($_GET['maxwidth'])  && preg_match('/^\d+$/', $_GET['maxwidth'] ))) $_GET['maxwidth']  = FALSE;
+			if ( ! (isset($_GET['maxheight']) && preg_match('/^\d+$/', $_GET['maxheight']))) $_GET['maxheight'] = FALSE;
+
+			// Find out new dimensions
+			if ($_GET['maxwidth'] && $_GET['maxheight'] && ! $_GET['height'] && ! $_GET['width'])
 			{
-				$this->response->headers('Content-Type', 'content-type: '.$mime.'; encoding='.Kohana::$charset.';');
+				if (($_GET['maxwidth'] / $_GET['maxheight']) < $wh_ratio) $_GET['width']  = $_GET['maxwidth'];
+				else                                                      $_GET['height'] = $_GET['maxheight'];
+			}
+			elseif ($_GET['maxwidth'] && ! $_GET['maxheight'] && ! $_GET['height'] && ! $_GET['width'])
+				$_GET['width'] = $_GET['maxwidth'];
+			elseif ( ! $_GET['maxwidth'] && $_GET['maxheight'] && ! $_GET['height'] && ! $_GET['width'])
+				$_GET['height'] = $_GET['maxheight'];
 
-				// Getting headers sent by the client.
-				$headers = apache_request_headers();
+			if ($_GET['height'] && ! $_GET['width']) $_GET['width']  = round($wh_ratio * $_GET['height']);
+			if ($_GET['width'] && ! $_GET['height']) $_GET['height'] = round($_GET['width'] / $wh_ratio);
 
-				$this->response->headers('Last-Modified', gmdate('D, d M Y H:i:s', filemtime($file)).' GMT');
+			if ( ! $_GET['width'] && ! $_GET['height'])
+			{
+				$_GET['height'] = $original_height;
+				$_GET['width']  = $original_width;
+			}
 
-				// Checking if the client is validating his cache and if it is current.
-				if (isset($headers['If-Modified-Since']) && (strtotime($headers['If-Modified-Since']) == filemtime($file)))
-				{
-					// Client's cache IS current, so we just respond '304 Not Modified'.
-					$this->response->status(304);
-				}
+			if ($_GET['width']  != $original_width)  $cache_ending .= '_width_'.$_GET['width'];
+			if ($_GET['height'] != $original_height) $cache_ending .= '_height_'.$_GET['height'];
+
+			$cached_filename = FALSE;
+			if ($cache_ending != '')
+			{
+				// Resizing needed
+				if (substr($path_info['dirname'], 0, 1) == '/')
+					$cached_filename = Kohana::$cache_dir.'/user_content'.$path_info['dirname'].'/'.$path_info['basename'].$cache_ending;
+				elseif ($path_info['dirname'] == '.')
+					$cached_filename = Kohana::$cache_dir.'/user_content/'.$path_info['basename'].$cache_ending;
 				else
+					$cached_filename = Kohana::$cache_dir.'/user_content/'.$path_info['dirname'].'/'.$path_info['basename'].$cache_ending;
+
+				if ( ! file_exists($cached_filename) || filemtime($file) < filemtime($cached_filename))
 				{
-					// Image not cached or cache outdated, we respond '200 OK' and output the image.
-					$this->response->headers('Content-Length', strval(filesize($file)));
-					$this->response->status(200);
-					echo file_get_contents($file);
+					if ( ! file_exists(pathinfo($cached_filename, PATHINFO_DIRNAME)))
+						exec('mkdir -p '.pathinfo($cached_filename, PATHINFO_DIRNAME));
+
+					// Create a new cached resized file
+					$this->resize_image($file, $cached_filename, $_GET['width'], $_GET['height']);
 				}
+			}
+
+			$this->response->headers('Content-Type', 'content-type: '.$mime.'; encoding='.Kohana::$charset.';');
+
+			// Getting headers sent by the client.
+			$headers = apache_request_headers();
+
+			if ($cached_filename)
+				$file = $cached_filename;
+
+			$this->response->headers('Last-Modified', gmdate('D, d M Y H:i:s', filemtime($file)).' GMT');
+
+
+			// Checking if the client is validating his cache and if it is current.
+			if (isset($headers['If-Modified-Since']) && (strtotime($headers['If-Modified-Since']) == filemtime($file)))
+			{
+				// Client's cache IS current, so we just respond '304 Not Modified'.
+				$this->response->status(304);
 			}
 			else
 			{
-				// This is not an image, so we respond that it is not found
-				throw new Http_Exception_404('File not found!');
+				// Image not cached or cache outdated, we respond '200 OK' and output the image.
+				$this->response->headers('Content-Length', strval(filesize($file)));
+				$this->response->status(200);
+				$this->response->body(file_get_contents($file));
 			}
 		}
 		else
@@ -112,131 +167,94 @@ class Controller_Media extends Controller
 
 	public function action_user_content_image()
 	{
-		$file = $this->request->param('file');
-
-		// Find the file ending
-		$file_parts  = explode('.', $file);
-		$file_ending = end($file_parts);
-		$filename    = $file;
-
-		// Check if it needs resizing
-		$cache_ending = '';
-
-		list($original_width, $original_height) = getimagesize(Kohana::$config->load('user_content.dir').'/images/'.$filename);
-		$wh_ratio = $original_width / $original_height;
-
-		// Get params
-		if (isset($_GET['width'])     && preg_match('/^\d+$/', $_GET['width'])) {}
-		else $_GET['width']     = FALSE;
-
-		if (isset($_GET['height'])    && preg_match('/^\d+$/', $_GET['height'])) {}
-		else $_GET['height']    = FALSE;
-
-		if (isset($_GET['maxwidth'])  && preg_match('/^\d+$/', $_GET['maxwidth'])) {}
-		else $_GET['maxwidth']  = FALSE;
-
-		if (isset($_GET['maxheight']) && preg_match('/^\d+$/', $_GET['maxheight'])) {}
-		else $_GET['maxheight'] = FALSE;
-
-		// Find out new dimensions
-		if ($_GET['maxwidth'] && $_GET['maxheight'] && ! $_GET['height'] && ! $_GET['width'])
+		$path      = $this->request->param('file');
+		$path_info = pathinfo($path);
+		$mime      = File::mime_by_ext($path_info['extension']);
+		$file      = Kohana::$config->load('user_content.dir').'/images/'.$path;
+		if ($file && substr($mime, 0, 5) == 'image')
 		{
-			if (($_GET['maxwidth'] / $_GET['maxheight']) < $wh_ratio) $_GET['width']  = $_GET['maxwidth'];
-			else                                                      $_GET['height'] = $_GET['maxheight'];
-		}
-		elseif ($_GET['maxwidth'] && ! $_GET['maxheight'] && ! $_GET['height'] && ! $_GET['width'])
-			$_GET['width'] = $_GET['maxwidth'];
-		elseif ( ! $_GET['maxwidth'] && $_GET['maxheight'] && ! $_GET['height'] && ! $_GET['width'])
-			$_GET['height'] = $_GET['maxheight'];
+			// Find the file ending
+			$file_ending = pathinfo($file, PATHINFO_EXTENSION);
 
-		if ($_GET['height'] && ! $_GET['width']) $_GET['width']  = round($wh_ratio * $_GET['height']);
-		if ($_GET['width'] && ! $_GET['height']) $_GET['height'] = round($_GET['width'] / $wh_ratio);
+			// Check if it needs resizing
+			$cache_ending = '';
 
-		if ( ! $_GET['width'] && ! $_GET['height'])
-		{
-			$_GET['height'] = $original_height;
-			$_GET['width']  = $original_width;
-		}
+			list($original_width, $original_height) = getimagesize($file);
+			$wh_ratio = $original_width / $original_height;
 
-		if ($_GET['width']  != $original_width)  $cache_ending .= '_width_'.$_GET['width'];
-		if ($_GET['height'] != $original_height) $cache_ending .= '_height_'.$_GET['height'];
+			// Get params
+			if ( ! (isset($_GET['width'])     && preg_match('/^\d+$/', $_GET['width']    ))) $_GET['width']     = FALSE;
+			if ( ! (isset($_GET['height'])    && preg_match('/^\d+$/', $_GET['height']   ))) $_GET['height']    = FALSE;
+			if ( ! (isset($_GET['maxwidth'])  && preg_match('/^\d+$/', $_GET['maxwidth'] ))) $_GET['maxwidth']  = FALSE;
+			if ( ! (isset($_GET['maxheight']) && preg_match('/^\d+$/', $_GET['maxheight']))) $_GET['maxheight'] = FALSE;
 
-		if ($cache_ending != '')
-		{
-			// Resizing needed
-
-			// Make sure the cache dir exists
-			$filename_parts = explode('/', $filename);
-			unset($filename_parts[count($filename_parts) - 1]);
-
-			$dir_to_create = Kohana::$cache_dir.'/user_content/images';
-			foreach ($filename_parts as $new_dir) $dir_to_create .= '/'.$new_dir;
-
-			$filename_w_path = Kohana::$config->load('user_content.dir').'/images/'.$filename;
-			$file            = Kohana::$cache_dir.'/user_content/images/'.$filename.$cache_ending;
-			if ( ! file_exists($file) || filemtime($file) < filemtime($filename_w_path))
+			// Find out new dimensions
+			if ($_GET['maxwidth'] && $_GET['maxheight'] && ! $_GET['height'] && ! $_GET['width'])
 			{
-				exec('mkdir -p '.$dir_to_create);
-				exec('chmod -R a+w '.Kohana::$cache_dir.'/user_content/images'); // Make sure its writeable by all
-
-				// Create a new cached resized file
-				if ($file_ending == 'jpg' || $file_ending == 'jpeg')
-				{
-					$src = imagecreatefromjpeg($filename_w_path);
-					$dst = imagecreatetruecolor($_GET['width'], $_GET['height']);
-					imagecopyresampled($dst, $src, 0, 0, 0, 0, $_GET['width'], $_GET['height'], $original_width, $original_height);
-					imagejpeg($dst, $file);
-				}
-				elseif ($file_ending == 'png')
-				{
-					$src = imagecreatefrompng($filename_w_path);
-					$dst = imagecreatetruecolor($_GET['width'], $_GET['height']);
-					imagecopyresampled($dst, $src, 0, 0, 0, 0, $_GET['width'], $_GET['height'], $original_width, $original_height);
-					imagepng($dst, $file);
-				}
-				/* Somethings fucked up with the colors in GIFs...
-				elseif ($file_ending == 'gif')
-				{
-					$src = imagecreatefromgif(Kohana::$config->load('user_content.dir').'/images/'.$filename);
-					$dst = imagecreatetruecolor($calculated_width, $calculated_height);
-					imagecopyresampled($dst, $src, 0, 0, 0, 0, $calculated_width, $calculated_height, $original_width, $original_height);
-					imagegif($dst, $file);
-				}
-				*/
+				if (($_GET['maxwidth'] / $_GET['maxheight']) < $wh_ratio) $_GET['width']  = $_GET['maxwidth'];
+				else                                                      $_GET['height'] = $_GET['maxheight'];
 			}
-		}
-		else $file = Kohana::$config->load('user_content.dir').'/images/'.$file;
+			elseif ($_GET['maxwidth'] && ! $_GET['maxheight'] && ! $_GET['height'] && ! $_GET['width'])
+				$_GET['width'] = $_GET['maxwidth'];
+			elseif ( ! $_GET['maxwidth'] && $_GET['maxheight'] && ! $_GET['height'] && ! $_GET['width'])
+				$_GET['height'] = $_GET['maxheight'];
 
-		if (file_exists($file))
-		{
-			$mime = File::mime_by_ext($file_ending);
-			if (substr($mime, 0, 5) == 'image')
+			if ($_GET['height'] && ! $_GET['width']) $_GET['width']  = round($wh_ratio * $_GET['height']);
+			if ($_GET['width'] && ! $_GET['height']) $_GET['height'] = round($_GET['width'] / $wh_ratio);
+
+			if ( ! $_GET['width'] && ! $_GET['height'])
 			{
-				$this->response->headers('Content-Type', 'content-type: '.$mime.'; encoding='.Kohana::$charset.';');
+				$_GET['height'] = $original_height;
+				$_GET['width']  = $original_width;
+			}
 
-				// Getting headers sent by the client.
-				$headers = apache_request_headers();
+			if ($_GET['width']  != $original_width)  $cache_ending .= '_width_'.$_GET['width'];
+			if ($_GET['height'] != $original_height) $cache_ending .= '_height_'.$_GET['height'];
 
-				$this->response->headers('Last-Modified', gmdate('D, d M Y H:i:s', filemtime($file)).' GMT');
-
-				// Checking if the client is validating his cache and if it is current.
-				if (isset($headers['If-Modified-Since']) && (strtotime($headers['If-Modified-Since']) == filemtime($file)))
-				{
-					// Client's cache IS current, so we just respond '304 Not Modified'.
-					$this->response->status(304);
-				}
+			$cached_filename = FALSE;
+			if ($cache_ending != '')
+			{
+				// Resizing needed
+				if (substr($path_info['dirname'], 0, 1) == '/')
+					$cached_filename = Kohana::$cache_dir.'/user_content'.$path_info['dirname'].'/'.$path_info['basename'].$cache_ending;
+				elseif ($path_info['dirname'] == '.')
+					$cached_filename = Kohana::$cache_dir.'/user_content/'.$path_info['basename'].$cache_ending;
 				else
+					$cached_filename = Kohana::$cache_dir.'/user_content/'.$path_info['dirname'].'/'.$path_info['basename'].$cache_ending;
+
+				if ( ! file_exists($cached_filename) || filemtime($file) < filemtime($cached_filename))
 				{
-					// Image not cached or cache outdated, we respond '200 OK' and output the image.
-					$this->response->headers('Content-Length', strval(filesize($file)));
-					$this->response->status(200);
-					echo file_get_contents($file);
+					if ( ! file_exists(pathinfo($cached_filename, PATHINFO_DIRNAME)))
+						exec('mkdir -p '.pathinfo($cached_filename, PATHINFO_DIRNAME));
+
+					// Create a new cached resized file
+					$this->resize_image($file, $cached_filename, $_GET['width'], $_GET['height']);
 				}
+			}
+
+			$this->response->headers('Content-Type', 'content-type: '.$mime.'; encoding='.Kohana::$charset.';');
+
+			// Getting headers sent by the client.
+			$headers = apache_request_headers();
+
+			if ($cached_filename)
+				$file = $cached_filename;
+
+			$this->response->headers('Last-Modified', gmdate('D, d M Y H:i:s', filemtime($file)).' GMT');
+
+
+			// Checking if the client is validating his cache and if it is current.
+			if (isset($headers['If-Modified-Since']) && (strtotime($headers['If-Modified-Since']) == filemtime($file)))
+			{
+				// Client's cache IS current, so we just respond '304 Not Modified'.
+				$this->response->status(304);
 			}
 			else
 			{
-				// This is not an image, so we respond that it is not found
-				throw new Http_Exception_404('File not found!');
+				// Image not cached or cache outdated, we respond '200 OK' and output the image.
+				$this->response->headers('Content-Length', strval(filesize($file)));
+				$this->response->status(200);
+				$this->response->body(file_get_contents($file));
 			}
 		}
 		else
@@ -260,6 +278,47 @@ class Controller_Media extends Controller
 		{
 			throw new Http_Exception_404('File not found!');
 		}
+	}
+
+	protected function resize_image($src, $dst, $new_width, $new_height)
+	{
+		if ($image_size = getimagesize($src))
+			list($original_width, $original_height) = $image_size;
+		else return FALSE;
+
+		$type = strtolower(substr(strrchr($src,'.'),1));
+		if ($type == 'jpeg') $type = 'jpg';
+		switch ($type)
+		{
+			case 'bmp': $img = imagecreatefromwbmp($src); break;
+			case 'gif': $img = imagecreatefromgif($src);  break;
+			case 'jpg': $img = imagecreatefromjpeg($src); break;
+			case 'png': $img = imagecreatefrompng($src);  break;
+			default : return FALSE;
+		}
+
+		// resize
+		$new_image = imagecreatetruecolor($new_width, $new_height);
+
+		// preserve transparency
+		if ($type == 'gif' or $type == 'png')
+		{
+			imagecolortransparent($new_image, imagecolorallocatealpha($new_image, 0, 0, 0, 127));
+			imagealphablending($new_image, FALSE);
+			imagesavealpha($new_image, TRUE);
+		}
+
+		imagecopyresampled($new_image, $img, 0, 0, 0, 0, $new_width, $new_height, $original_width, $original_height);
+
+		switch ($type)
+		{
+			case 'bmp': imagewbmp($new_image, $dst); break;
+			case 'gif': imagegif($new_image,  $dst); break;
+			case 'jpg': imagejpeg($new_image, $dst); break;
+			case 'png': imagepng($new_image,  $dst); break;
+		}
+
+		return TRUE;
 	}
 
 }
